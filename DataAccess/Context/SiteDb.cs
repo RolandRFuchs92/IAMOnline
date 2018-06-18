b707b7615aa8c1f09260e175009042880c0561bc
Roland Fuchs <roland@ix.co.za>
2018-06-18 13:09:19 +0200

    Added the rest of the entities to the DbContext

--- DataAccess/Context/SiteDb.cs
+++ DataAccess/Context/SiteDb.cs
@@ -8,15 +8,21 @@ using DataAccess.Entities;
 
 namespace DataAccess.Context
 {
-	public class SiteDb : DbContext
-	{
-		public SiteDb(DbContextOptions<SiteDb> options) : base(options)
+		public class SiteDb : DbContext
 		{
+				public SiteDb(DbContextOptions<SiteDb> options) : base(options)
+				{
+				}
+
+				public DbSet<Address> Addresses { get; set; }
+				public DbSet<AddressCountry> AddressCountries { get; set; }
+				public DbSet<AddressProvince> AddressProvinces { get; set; }
+				public DbSet<AddressStreet> AddressStreets { get; set; }
+				public DbSet<BlogDetail> BlogDetails { get; set; }
+				public DbSet<BlogType> BlogTypes { get; set; }
+				public DbSet<Client> Clients { get; set; }
+				public DbSet<CoreBlog> CoreBlogs { get; set; }
+				public DbSet<CoreMember> CoreMembers { get; set; }
+				public DbSet<Person> Persons { get; set; }
 		}
-
-		public DbSet<BlogType> BlogTypes { get; set; }
-		public DbSet<BlogDetail> BlogDetails { get; set; }
-		public DbSet<AddressCountry> AddressCountries{ get; set; }
-
-	}
 }
