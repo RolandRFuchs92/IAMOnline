7f8c2e1ed85f178cee2a533457ba2492d30b2881
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-07-20 18:37:29 +0200

    Removed migrations

---
+++ Iamonline.Context/IamonlineContext.Factory.cs
@@ -0,0 +1,17 @@
+﻿using Iamonline.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Iamonline.Data
+{
+	public class SiteDbFactory : IDesignTimeDbContextFactory<IamonlineContext>
+	{
+		public IamonlineContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<IamonlineContext>();
+			optionsBuilder.UseSqlServer("server=MSI\\SQLEXPRESS;Database=SiteDb;Integrated Security=true;MultipleActiveResultSets=true;");
+
+			return new IamonlineContext(optionsBuilder.Options);
+		}
+	}
+}
