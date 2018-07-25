38f18f1efb6745d280823339c21b716eecc8000b
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-07-25 22:03:02 +0200

    First steps to getting react app working!

---
+++ Iamonline/wwwroot/iamonline/src/App.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import { BS4Text } from './Shared/ShorthandBootstrap.jsx';
+import { Form } from 'reactstrap';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="col-lg-4">
+					<h4>Checkout this cool little form bru!</h4>
+
+					<Form>
+						<BS4Text />
+						
+					</Form>
+				</div>
+			</div>
+		);
+	}
+}
+
---
+++ Iamonline/wwwroot/iamonline/src/Shared/ShorthandBootstrap.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import { FormGroup, Label, Input } from 'reactstrap';
+
+export class BS4Text extends React.Component {
+	render() {
+		return (
+			<FormGroup>
+				<Label for="username">Username</Label>
+				<Input type="text" name="username" id="username" placeholder="Username" />
+			</FormGroup>
+			);
+	}
+}
